e8f4a258ce874b67a4346fba7d5d85f2a97842a8
Ebube Nwadiokwu <111129501+Ebunwadi@users.noreply.github.com>
2023-08-09 00:24:20 +0100

    Update user.js
    fix issue#1

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 // get a user and the events he created
 module.exports.getUser = async (req, res) => {
@@ -6,14 +7,14 @@ module.exports.getUser = async (req, res) => {
     { _id: req.params.id },
     "-tickets",
   ).populate("events");
-  if (!getUser) return res.status(400).json({ message: "user not found" });
+  if (!getUser) return res.status(200).json({ message: "user not found" });
   return res.json({ data: getUser, success: true });
 };
 
 // get users and their purchased tickets
 module.exports.getUsersTickets = async (req, res) => {
   const getUser = await User.find().lean();
-  if (!getUser) return res.status(400).json({ message: "user not found" });
+  if (!getUser) return res.status(200).json({ message: "user not found" });
   const result = getUser.map((user) => ({
     name: user.name,
     tickets: user.tickets,
@@ -24,7 +25,7 @@ module.exports.getUsersTickets = async (req, res) => {
 // get users and events
 module.exports.getUsersEvents = async (req, res) => {
   const getUser = await User.find().populate("events");
-  if (!getUser) return res.status(400).json({ message: "user not found" });
+  if (!getUser) return res.status(200).json({ message: "user not found" });
   const result = getUser.map((user) => ({
     name: user.name,
     events: user.events,
@@ -35,14 +36,14 @@ module.exports.getUsersEvents = async (req, res) => {
 //get all the event of the current user
 module.exports.getCurrentUserEvents = async (req, res) => {
   const getUser = await User.findOne({ _id: req.user._id }).populate("events");
-  if (!getUser) return res.status(400).json({ message: "user not found" });
+  if (!getUser) return res.status(200).json({ message: "user not found" });
   return res.json({ data: getUser, success: true });
 };
 
 // get user profile
 module.exports.getUserProfile = async (req, res) => {
   const getUser = await User.findOne({ _id: req.user._id }, "-password");
-  if (!getUser) return res.status(400).json({ message: "user not found" });
+  if (!getUser) return res.status(200).json({ message: "user not found" });
   return res.json({ data: getUser, success: true });
 };
 
@@ -55,7 +56,7 @@ module.exports.updateUserProfile = async (req, res) => {
     { $set: req.body },
     { new: true },
   );
-  if (!getUser) return res.status(400).json({ message: "user not found" });
+  if (!getUser) return res.status(200).json({ message: "user not found" });
   return res.json({ data: getUser, success: true });
 };
 
@@ -63,12 +64,13 @@ module.exports.updateUserProfile = async (req, res) => {
 module.exports.updateUserPassword = async (req, res) => {
   if (req.body.name || req.body.email)
     return res.status(400).json("you can only update password");
+  const hashedPwd = await bcrypt.hash(req.body.password, 10); // salt rounds
   const getUser = await User.findOneAndUpdate(
     { _id: req.user._id },
-    { $set: req.body },
+    { password: hashedPwd },
     { new: true },
   );
-  if (!getUser) return res.status(400).json({ message: "user not found" });
+  if (!getUser) return res.status(200).json({ message: "user not found" });
 
   // logout the user
   res.clearCookie("auth", { httpOnly: true, sameSite: "None", secure: true });
